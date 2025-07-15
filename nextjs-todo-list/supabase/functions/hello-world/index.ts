28893bdfd658dfa501e4abef4ac552c9091b2162
xsahxl <xsahxl@126.com>
2025-07-15 18:51:17 +0800

    fix: vercel build error

--- nextjs-todo-list/supabase/functions/hello-world/index.ts
+++ nextjs-todo-list/supabase/functions/hello-world/index.ts
@@ -6,7 +6,7 @@
 import "jsr:@supabase/functions-js/edge-runtime.d.ts"
 
 console.log("Hello from Functions!")
-
+// @ts-expect-error Deno global is provided at runtime
 Deno.serve(async (req) => {
   const { name } = await req.json()
   const data = {
